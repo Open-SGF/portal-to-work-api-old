8e46822150f8b1d5428fd612170594e60f39dff4
Frederick Lawler <fred@fredlawl.com>
2021-05-26 19:33:39 -0500

    Replace Spatie CORS middleware with custom CORS middleware
    Commit ed94b5b ("Laravel CORS") introduced CORS into the project. Spatie
    doesn't actually pass back the headers we were hoping. Instead, we'll
    use our own settings instead.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Spatie\Cors\Cors::class,
+            \App\Http\Middleware\Cors::class
         ],
     ];
 
